2e7c54cb202eefbe93a7d8a6cccaf3f738938247
Brad B <brad.bambara@smallplanet.com>
2014-04-15 17:11:24 -0400

    adding step 2: snapshot API

---
+++ Step2_snapshotAPI/TransitionTutorial/TransitionTutorial/Transitions/TTPopAnimator.h
@@ -0,0 +1,13 @@
+//
+//  TTPopAnimator.h
+//  TransitionTutorial
+//
+//  Created by Brad Bambara on 4/14/14.
+//  Copyright (c) 2014 Brad Bambara. All rights reserved.
+//
+
+@import Foundation;
+
+@interface TTPopAnimator : NSObject <UIViewControllerAnimatedTransitioning>
+
+@end
