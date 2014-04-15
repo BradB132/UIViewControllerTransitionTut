c245aa3f504b002ab3e13739826a206e88db214c
Brad B <brad.bambara@smallplanet.com>
2014-04-15 17:05:15 -0400

    adding step 1: crossfade

---
+++ Step1_crossfade/TransitionTutorial/TransitionTutorial/Controllers/TTViewController.m
@@ -0,0 +1,43 @@
+//
+//  TTNavigationController.m
+//  TransitionTutorial
+//
+//  Created by Brad Bambara on 4/14/14.
+//  Copyright (c) 2014 Brad Bambara. All rights reserved.
+//
+
+#import "TTViewController.h"
+#import "TTPushAnimator.h"
+
+@interface TTViewController ()
+
+@end
+
+@implementation TTViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	
+	self.navigationController.delegate = self;
+}
+
+#pragma mark - UINavigationControllerDelegate
+
+- (id <UIViewControllerAnimatedTransitioning>)navigationController:(UINavigationController *)navigationController
+                                   animationControllerForOperation:(UINavigationControllerOperation)operation
+                                                fromViewController:(UIViewController *)fromVC
+                                                  toViewController:(UIViewController *)toVC
+{
+	switch(operation)
+	{
+		case UINavigationControllerOperationPush:
+			return [[TTPushAnimator alloc] init];
+		case UINavigationControllerOperationPop:
+			return [[TTPushAnimator alloc] init];
+		default:
+			return nil;
+	}
+}
+
+@end
